df7b5a1bc2385ced9c854202ea4d6c3575b6e80f
MartinPorciuncula <kgtprav@gmail.com>
2023-09-14 15:17:10 -0300

    Fix bugs and styling desktop mode component Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,17 +14,21 @@ const Contact = () => {
       });
   };
   return (
-   <section className=''>
-     <form className='flex flex-col justify-center items-center gap-2 p-4' onSubmit={sendEmail}>
+   <section className='flex justify-center items-center gap-4 flex-row'>
+     <form  className='flex flex-col justify-center items-center gap-2 p-4 bg-bg-100/40  min-[400px]:h-96 w-96  min-[400px]:flex-col' onSubmit={sendEmail}>
       <label className='font-bold text-xl'>Name</label>
       <input placeholder='Type Here Your Name' type="text" name="user_name" />
       <label className='font-bold text-xl'>Email</label>
       <input  placeholder='Type Here Your Email' type="email" name="user_email" />
       <label  className='font-bold text-xl'>Message</label>
-      <textarea name="message" />
+      <textarea  placeholder="Type Here Your Message to contact me from Email" name="message" />
       <input className='bg-try py-1 rounded-lg cursor-pointer px-6 hover:scale-125 transition-all hover:animate-pulse font-semibold' type="submit" value="Send" />
     </form>
-   </section>
+    <article className='flex flex-col gap-6  min-[400px]:flex'>
+    <a className='bg-primary2 px-6 rounded-lg  hover:scale-125  min-[400px]:w-20 hover:transition-all' href="https://w.app/c2JkuC" target='_blank'> <i className='bx bxl-whatsapp text-3xl'></i></a>  
+    <a className='bg-primary2 px-6 rounded-lg hover:scale-125 min-[400px]:w-20 hover:transition-all'  href="https://w.app/c2JkuC" target='_blank'> <i class='bx bxl-linkedin-square text-3xl'></i> </a>
+    </article>
+  </section>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <article className="text-text-white  w-full p-2">
       <article id="Home" className="  w-full flex justify-between h-full pt-12 relative min-[400px]:flex  min-[400px]:justify-center">
-        <section className="p-2 z-10 bg-bg-100/50 grid gap-2 w-[90%] min-[400px]:h-56 min-[400px]:w-[50%] min-[400px]:flex min-[400px]:justify-center min-[400px]:items-center min-[400px]:flex-col" >
+        <section className="p-2 z-10 bg-bg-100/50 grid gap-2 w-[90%] min-[400px]:h-56 min-[400px]:w-[70%] min-[400px]:flex min-[400px]:justify-center min-[400px]:items-center min-[400px]:flex-col" >
           <h3 className="text-1xl font-semibold mb-1 min-[400px]:text-2xl">Welcome,</h3>
           <h2 className="text-2xl font-bold min-[400px]:text-3xl">To my briefcase,Traveler</h2>
           <a href="#Contact" className="mt-4 place-self-start px-4 border rounded-full text-lg hover:scale-110 hover:animate-pulse min-[400px]:flex min-[400px]:place-self-center">
